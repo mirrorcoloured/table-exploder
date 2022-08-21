cef9f419c14686720aed0b96f55a3b23a9c8cb69
Sky C <mirrorcoloured@gmail.com>
2022-08-20 19:27:17 -0600

    switching to plotly, not working yet

--- ExplodableDataFrame.py
+++ ExplodableDataFrame.py
@@ -2,6 +2,7 @@ from itertools import combinations
 
 import pandas as pd
 import networkx as nx
+import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import openpyxl # used as an engine in pandas, import not actually needed
 
@@ -238,24 +239,108 @@ class ExplodableDataFrame(pd.DataFrame):
             return unique_two_columns
 
 
-    def visualize_relationships(self):
+    def relationships_to_graph(self):
         rel = self.detect_column_relationships()
         G = nx.DiGraph()
         G.add_nodes_from(rel["Independent"])
         G.add_nodes_from(rel["Constant"])
         G.add_nodes_from(rel["Unique"])
         G.add_edges_from(rel["Identifies"])
-        options = {
-            "font_size": 20,
-            "width": 3,
-            "arrowsize": 20,
-            "node_color": "gray",
-            "edge_color": "red",
-        }
-        nx.draw_networkx(G, pos=nx.spring_layout(G), **options)
-        ax = plt.gca()
-        ax.margins(0.20)
-        plt.show()
+        for group in rel["Bijective"]:
+            G.add_edges_from(combinations(group, 2)) # TODO bidirectional arrows
+        return G
+    
+    def visualize_relationships(self):
+        G = self.relationships_to_graph()
+        # options = {
+        #     "font_size": 20,
+        #     "width": 3,
+        #     "arrowsize": 20,
+        #     "node_color": "gray",
+        #     "edge_color": "red",
+        # }
+        # nx.draw_networkx(G, pos=nx.spring_layout(G), **options)
+        # ax = plt.gca()
+        # ax.margins(0.20)
+        # plt.show()
+
+        pos=nx.spring_layout(G)
+        
+        # https://plotly.com/python/network-graphs/
+        # G = nx.random_geometric_graph(200, 0.125)
+        
+        edge_x = []
+        edge_y = []
+        for edge in G.edges():
+            x0, y0 = G.nodes[edge[0]]['pos']
+            x1, y1 = G.nodes[edge[1]]['pos']
+            edge_x.append(x0)
+            edge_x.append(x1)
+            edge_x.append(None)
+            edge_y.append(y0)
+            edge_y.append(y1)
+            edge_y.append(None)
+
+        edge_trace = go.Scatter(
+            x=edge_x, y=edge_y,
+            line=dict(width=0.5, color='#888'),
+            hoverinfo='none',
+            mode='lines')
+
+        node_x = []
+        node_y = []
+        for node in G.nodes():
+            x, y = G.nodes[node]['pos']
+            node_x.append(x)
+            node_y.append(y)
+
+        node_trace = go.Scatter(
+            x=node_x, y=node_y,
+            mode='markers',
+            hoverinfo='text',
+            marker=dict(
+                showscale=True,
+                # colorscale options
+                #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
+                #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
+                #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
+                colorscale='YlGnBu',
+                reversescale=True,
+                color=[],
+                size=10,
+                colorbar=dict(
+                    thickness=15,
+                    title='Node Connections',
+                    xanchor='left',
+                    titleside='right'
+                ),
+                line_width=2))
+        
+        node_adjacencies = []
+        node_text = []
+        for node, adjacencies in enumerate(G.adjacency()):
+            node_adjacencies.append(len(adjacencies[1]))
+            node_text.append('# of connections: '+str(len(adjacencies[1])))
+
+        node_trace.marker.color = node_adjacencies
+        node_trace.text = node_text
+
+        fig = go.Figure(data=[edge_trace, node_trace],
+             layout=go.Layout(
+                title='<br>Network graph made with Python',
+                titlefont_size=16,
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20,l=5,r=5,t=40),
+                # annotations=[ dict(
+                #     text="Python code: <a href='https://plotly.com/ipython-notebooks/network-graphs/'> https://plotly.com/ipython-notebooks/network-graphs/</a>",
+                #     showarrow=False,
+                #     xref="paper", yref="paper",
+                #     x=0.005, y=-0.002 ) ],
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                )
+        fig.show()
 
 
 def remove_if_present(list_obj, value):
